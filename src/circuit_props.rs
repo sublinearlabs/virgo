cfecb1799c2efe25b8b72cb8c07332a5aa2fbbab
developeruche <developeruche@gmail.com>
2025-05-28 21:18:25 +0100

    Circuit props init trait

---
+++ src/circuit_props.rs
@@ -0,0 +1,7 @@
+/// Vigro GKR Circuit properties.
+
+/// This trait defines the properties of a Vigro GKR Circuit.
+/// impl on the Circuit, enabling the query of add_mle, mle_mle and v_poly.
+pub trait CircuitProps {
+    
+}

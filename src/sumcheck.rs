c4ca3a889520a6e96ca51e25a8b738932abdcb50
Wisdom Ogwu <iammadab@gmail.com>
2025-06-14 17:54:54 +0100

    add the sumcheck crate from sl-core

---
+++ src/sumcheck.rs
@@ -0,0 +1,7 @@
+use sum_check::primitives::SumCheckProof;
+
+use crate::util::LayerProvingInfoWithSubset;
+
+fn phase_one<F>(layer_proving_info: &LayerProvingInfoWithSubset<F>) -> SumCheckProof<F, E> {
+    todo!()
+}

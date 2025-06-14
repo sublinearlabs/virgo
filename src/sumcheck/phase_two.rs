e04228083ef1847e155c7778837846c058924f78
Wisdom Ogwu <iammadab@gmail.com>
2025-06-14 23:47:23 +0100

    cleanup utils file

--- src/sumcheck/phase_two.rs
+++ src/sumcheck/phase_two.rs
@@ -2,6 +2,8 @@ use p3_field::{ExtensionField, Field, PrimeField32};
 use sum_check::primitives::SumCheckProof;
 
 // how do we handle phase 2??
+// we need the first set of challenges
+// then we evaluate the first subset at that challenge point
 
 pub(crate) fn prove_phase_two<F: Field + PrimeField32, E: ExtensionField<F>>() -> SumCheckProof<F, E>
 {

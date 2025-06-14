f8104df517a2ea96047a962d5db897466f913dd2
Wisdom Ogwu <iammadab@gmail.com>
2025-06-14 18:40:48 +0100

    implement product book keep table

--- src/sumcheck.rs
+++ src/sumcheck.rs
@@ -11,6 +11,7 @@ use crate::util::LayerProvingInfoWithSubset;
 //
 // now I need a function that represents phase 1
 
+// TODO: add proper documentation
 fn prove_phase_one<F: Field, E: ExtensionField<F>>(
     layer_proving_info: &LayerProvingInfoWithSubset<Fields<F, E>>,
 ) -> SumCheckProof<F, E> {
@@ -29,17 +30,31 @@ fn prove_phase_one<F: Field, E: ExtensionField<F>>(
     todo!()
 }
 
+// TODO: add proper documentation
 fn build_product_bookkeeping_table<F: Field, E: ExtensionField<F>>(
+    igz: &[Fields<F, E>],
     sparse_entries: &[Vec<[usize; 3]>],
     subsets: Vec<Vec<Fields<F, E>>>,
 ) -> Vec<Fields<F, E>> {
+    // ensure there is one sparse entry for each subset
+    debug_assert_eq!(sparse_entries.len(), subsets.len());
+
     // we need something in the size of x, how do we know the size of x?
     // is that the same as the size of the first subset??
 
     // the size of the table is based on the size of the first subset vector
     // as the first subset vector is also the common vector for all layers
-    let mut table: Vec<Fields<F, E>> = vec![Fields::Base(F::zero()); subsets[0].len()];
-    todo!()
+    let mut table = vec![Fields::Base(F::zero()); subsets[0].len()];
+
+    // next we need to iterate over each (entry, subset) pair
+    // and use that to populate the table
+    for (sparse_entry, subset) in sparse_entries.iter().zip(subsets) {
+        for [z, x, y] in sparse_entry {
+            table[*x] += igz[*z] * subset[*y];
+        }
+    }
+
+    table
 }
 
 /// Utility function to merge two sumcheck proofs

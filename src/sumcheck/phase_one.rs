1465dcf899e5214a0a06607c7466aaf32d473ba8
Wisdom Ogwu <iammadab@gmail.com>
2025-06-18 20:33:47 +0100

    allow dead code + fmt

--- src/sumcheck/phase_one.rs
+++ src/sumcheck/phase_one.rs
@@ -1,10 +1,10 @@
 use std::rc::Rc;
 
 use p3_field::{ExtensionField, Field, PrimeField32};
-use poly::{mle::MultilinearPoly, vpoly::VPoly, Fields};
+use poly::{Fields, mle::MultilinearPoly, vpoly::VPoly};
+use sum_check::SumCheck;
 use sum_check::interface::SumCheckInterface;
 use sum_check::primitives::SumCheckProof;
-use sum_check::SumCheck;
 use transcript::Transcript;
 
 use crate::util::LayerProvingInfoWithSubset;
@@ -91,17 +91,4 @@ fn build_bookkeeping_table_with_identity<F: Field, E: ExtensionField<F>>(
         }
     }
     table
-}
-
-/// Utility function to merge two sumcheck proofs
-/// used to merge the phase 1 and phase 2 sumcheck proofs
-fn merge_sumcheck_proofs<F: Field, E: ExtensionField<F>>(
-    proofs: [SumCheckProof<F, E>; 2],
-) -> SumCheckProof<F, E> {
-    let [proof1, proof2] = proofs;
-    SumCheckProof {
-        claimed_sum: proof1.claimed_sum,
-        round_polynomials: [proof1.round_polynomials, proof2.round_polynomials].concat(),
-        challenges: [proof1.challenges, proof2.challenges].concat(),
-    }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,10 @@
 // use libra::utils::{build_phase_one_libra_sumcheck_poly, generate_eq, initialize_phase_one};
 use p3_field::{ExtensionField, Field};
 use poly::{
+    Fields, MultilinearExtension,
     mle::MultilinearPoly,
     utils::{generate_eq, product_poly},
     vpoly::VPoly,
-    Fields, MultilinearExtension,
 };
 use sum_check::interface::SumCheckInterface;
 
@@ -132,19 +132,19 @@ pub(crate) fn push_index<T: PartialEq>(container: &mut Vec<T>, item: T) -> usize
 mod tests {
     use std::vec;
 
-    use p3_field::{extension::BinomialExtensionField, AbstractField};
+    use p3_field::{AbstractField, extension::BinomialExtensionField};
     use p3_mersenne_31::Mersenne31;
     use poly::{
-        mle::MultilinearPoly, utils::product_poly, vpoly::VPoly, Fields, MultilinearExtension,
+        Fields, MultilinearExtension, mle::MultilinearPoly, utils::product_poly, vpoly::VPoly,
     };
-    use sum_check::{interface::SumCheckInterface, SumCheck};
+    use sum_check::{SumCheck, interface::SumCheckInterface};
     use transcript::Transcript;
 
     type F = Mersenne31;
     type E = BinomialExtensionField<F, 3>;
     type S = SumCheck<F, E, VPoly<F, E>>;
 
-    use crate::util::{build_agi, n_to_1_folding, Subclaim};
+    use crate::util::{Subclaim, build_agi, n_to_1_folding};
 
     #[test]
     fn test_n_to_1_folding() {

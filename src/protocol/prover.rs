58433bbecb52e027651af6ccce6a09e781ee51e6
Wisdom Ogwu <iammadab@gmail.com>
2025-06-20 18:32:03 +0100

    take transcript as a parameter to prover

--- src/protocol/prover.rs
+++ src/protocol/prover.rs
@@ -1,5 +1,6 @@
 use p3_field::{ExtensionField, Field};
 use poly::Fields;
+use transcript::Transcript;
 
 use crate::circuit::GeneralCircuit;
 
@@ -8,6 +9,7 @@ use super::VirgoProof;
 pub fn prove<F: Field, E: ExtensionField<F>>(
     circuit: &GeneralCircuit,
     evaluations: &[Vec<Fields<F, E>>],
+    transcript: Transcript<F, E>,
 ) -> VirgoProof<F, E> {
     todo!()
 }

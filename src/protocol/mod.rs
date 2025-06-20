ad508b80875d82262a40a79d9f1b05b77d23a320
Wisdom Ogwu <iammadab@gmail.com>
2025-06-20 17:50:23 +0100

    wip

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -4,9 +4,8 @@ use p3_field::{ExtensionField, Field};
 use poly::Fields;
 use sum_check::primitives::SumCheckProof;
 
-type Hint<F, E> = Fields<F, E>;
-type LayerSumcheck<F, E> = (SumCheckProof<F, E>, Vec<Hint<F, E>>);
-type FoldingSumcheck<F, E> = (SumCheckProof<F, E>, Hint<F, E>);
+type LayerSumcheck<F, E> = (SumCheckProof<F, E>, Vec<Fields<F, E>>);
+type FoldingSumcheck<F, E> = (SumCheckProof<F, E>, Fields<F, E>);
 
 pub struct VirgoProof<F: Field, E: ExtensionField<F>> {
     pub layer_sumchecks: Vec<LayerSumcheck<F, E>>,

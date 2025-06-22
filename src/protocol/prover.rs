59232e2c53c82e822371b3a0613666dc6d74776b
Wisdom Ogwu <iammadab@gmail.com>
2025-06-22 07:35:14 +0100

    add comments to prover

--- src/protocol/prover.rs
+++ src/protocol/prover.rs
@@ -12,41 +12,41 @@ use crate::{
 use super::VirgoProof;
 use crate::util::Subclaim;
 
+/// Prove the correct execution of a `GeneralCircuit`
 pub fn prove<F: Field + PrimeField32, E: ExtensionField<F>>(
     circuit: &GeneralCircuit,
     evaluations: &[Vec<Fields<F, E>>],
     transcript: &mut Transcript<F, E>,
 ) -> VirgoProof<F, E> {
-    // initialize empty proof
     let mut proof = VirgoProof::<F, E>::default();
-
-    // TODO: this might be just enough for collection of subclaims for the input layer
-    //  need to verify this
     let mut layer_subclaims: Vec<Vec<Subclaim<F, E>>> = vec![vec![]; circuit.layers.len()];
 
-    // commit the output mle to the transcript
+    // TODO: do we need to commit the circuit to the transcript?
+
+    // commit output to the transcript
     let output_mle =
         MultilinearPoly::new_extend_to_power_of_two(evaluations[0].clone(), Fields::from_u32(0));
     output_mle.commit_to_transcript(transcript);
 
-    // sample challenges for the output
-    let mut r = extension_to_fields(transcript.sample_n_challenges(output_mle.num_vars()));
-
-    // get layer evaluation
-    let mut m = output_mle.evaluate(r.as_slice());
+    // generate layer claim via challenge and evaluation
+    let mut eval_point = extension_to_fields(transcript.sample_n_challenges(output_mle.num_vars()));
+    let mut eval = output_mle.evaluate(eval_point.as_slice());
 
     for i in 0..circuit.layers.len() {
+        // get info needed to prove the current layer sumcheck relation
         let layer_proving_info = circuit
             .generate_layer_proving_info(i)
             .extract_subsets(evaluations);
 
-        // TODO: document subsection
-        let layer_sumcheck_proof = prove_sumcheck_layer(m, &r, &layer_proving_info, transcript);
+        // generate current layer sumcheck proof and
+        // generate oracle check hints
+        let layer_sumcheck_proof =
+            prove_sumcheck_layer(eval, &eval_point, &layer_proving_info, transcript);
         let subclaims = layer_proving_info.eval_subsets(&layer_sumcheck_proof.challenges);
         let hints = subclaims_to_hints(&subclaims);
 
-        // TODO: clean up this observation mechanism
-        //  there should be a way to just observe Fields
+        // send hints to proof
+        // TODO: fix transcript to simplify this step
         transcript.observe_ext_element(
             &hints
                 .iter()
@@ -55,10 +55,11 @@ pub fn prove<F: Field + PrimeField32, E: ExtensionField<F>>(
         );
         proof.add_layer_proof(layer_sumcheck_proof, hints);
 
-        // next we need to deposit subclaims
-        deposit_into_subset_info(&mut layer_subclaims, subclaims);
+        // distribute the subclaim to their appropriate layers
+        deposit_subclaims(&mut layer_subclaims, subclaims);
 
-        // sample alphas
+        // prepare the next layer
+        // we do this by folding all subclaims for the next layer into a single claim
         let alphas = extension_to_fields(transcript.sample_n_challenges(layer_subclaims[i].len()));
         let folding_proof: SumCheckProof<F, E> = n_to_1_folding(
             transcript,
@@ -69,48 +70,53 @@ pub fn prove<F: Field + PrimeField32, E: ExtensionField<F>>(
         .unwrap();
 
         // update the evaluation point and the eval
-        r = folding_proof.challenges.clone();
-        m = MultilinearPoly::new_extend_to_power_of_two(
+        eval_point = folding_proof.challenges.clone();
+        eval = MultilinearPoly::new_extend_to_power_of_two(
             evaluations[i + 1].clone(),
             Fields::from_u32(0),
         )
-        .evaluate(&r);
+        .evaluate(&eval_point);
 
-        proof.add_folding_proof(folding_proof, m);
+        proof.add_folding_proof(folding_proof, eval);
     }
 
     proof
 }
 
-// TODO: add documentation
-fn deposit_into_subset_info<T>(subset_info: &mut [Vec<T>], data: Vec<T>) {
-    debug_assert_eq!(subset_info.len() + 1, data.len());
+/// Distributes a set of subclaim belonging to different layers to their
+/// appropriate layer entry slot.
+fn deposit_subclaims<T>(subclaims_container: &mut [Vec<T>], subclaims: Vec<T>) {
+    debug_assert_eq!(subclaims_container.len() + 1, subclaims.len());
 
-    let mut data_iter = data.into_iter();
-    subset_info[0].push(data_iter.next().unwrap());
+    let mut subclaims_iter = subclaims.into_iter();
+    subclaims_container[0].push(subclaims_iter.next().unwrap());
 
-    for (entry, data) in subset_info.iter_mut().zip(data_iter) {
-        entry.push(data);
+    for (entry, subclaim) in subclaims_container.iter_mut().zip(subclaims_iter) {
+        entry.push(subclaim);
     }
 }
 
-// TODO: make the need for this reduandant
+/// Converts a set of extension fields elements to `Fields` type
+// TODO: this should be made redaundant once changes are made to `Transcript`
 fn extension_to_fields<F: Field, E: ExtensionField<F>>(vals: Vec<E>) -> Vec<Fields<F, E>> {
     vals.into_iter().map(|v| Fields::Extension(v)).collect()
 }
 
 #[cfg(test)]
 mod test {
-    use super::deposit_into_subset_info;
+    use super::deposit_subclaims;
 
     #[test]
-    fn test_deposit_subset_info() {
-        let mut subset_info = vec![vec![]; 3];
+    fn test_deposit_subclaims_container() {
+        let mut subclaims_container = vec![vec![]; 3];
 
-        deposit_into_subset_info(&mut subset_info, vec![1, 2, 3, 4]);
-        assert_eq!(subset_info, vec![vec![1, 2], vec![3], vec![4]]);
+        deposit_subclaims(&mut subclaims_container, vec![1, 2, 3, 4]);
+        assert_eq!(subclaims_container, vec![vec![1, 2], vec![3], vec![4]]);
 
-        deposit_into_subset_info(&mut subset_info[1..], vec![5, 6, 7]);
-        assert_eq!(subset_info, vec![vec![1, 2], vec![3, 5, 6], vec![4, 7]]);
+        deposit_subclaims(&mut subclaims_container[1..], vec![5, 6, 7]);
+        assert_eq!(
+            subclaims_container,
+            vec![vec![1, 2], vec![3, 5, 6], vec![4, 7]]
+        );
     }
 }
